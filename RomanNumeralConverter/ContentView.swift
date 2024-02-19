2ed089c52d51fd5c23adf50d1255d5f47fb931da
johnnyo <johnolszewskim@gmail.com>
2024-02-18 16:51:24 -0800

    Initial Commit

---
+++ RomanNumeralConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RomanNumeralConverter
+//
+//  Created by Johnny O on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RomanNumeralConverter/RomanNumeralConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RomanNumeralConverterApp.swift
+//  RomanNumeralConverter
+//
+//  Created by Johnny O on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RomanNumeralConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
